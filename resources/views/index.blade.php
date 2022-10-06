8e5eada3f232d68ccd512e0cda21e4ba0b647f78
Yassin Abdelhamid <yassin1001@gmail.com>
2022-10-06 17:58:37 +0200

    Created index blade and route

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <h1>Welcome Text</h1>
+        <p>Random blablablablablabalabalabalabablabalabalbalablablablalabablalbalbalbalabl</p>
+    </section>
+    <section>
+        <h1>Bars</h1>
+        <p>Random blablablablablabalabalabalabablabalabalbalablablablalabablalbalbalbalabl</p>
+    </section>
+    <section>
+        <h1>Restaurants</h1>
+        <p>Random blablablablablabalabalabalabablabalabalbalablablablalabablalbalbalbalabl</p>
+    </section>
+</body>
+</html>
