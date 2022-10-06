1b02341973480ecc769bc689059e43e365078630
Yassin Abdelhamid <yassin1001@gmail.com>
2022-10-06 18:15:43 +0200

    Custom css file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="de">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <title>BaselBars</title>
 </head>
+
 <body>
     <section>
         <h1>Welcome Text</h1>
@@ -20,4 +26,5 @@
         <p>Random blablablablablabalabalabalabablabalabalbalablablablalabablalbalbalbalabl</p>
     </section>
 </body>
+
 </html>
