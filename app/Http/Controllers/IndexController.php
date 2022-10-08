e4e378ec2edad02ab92eff7c4643153e5fca833f
Yassin Abdelhamid <yassin1001@gmail.com>
2022-10-08 20:21:44 +0200

    Created route and data output

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
-    
+    public function show() 
+    {
+        $bar = DB::table('bars')->select('name')->get();
+        return view('index')->with('bar', $bar);
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,8 +35,9 @@
             </section>
 
             <section>
+                @foreach ($bar as $bars)
                 <h1>Bars</h1>
-                <p>Random blablablablablabalabalabalabablabalabalbalablablablalabablalbalbalbalabl</p>
+                <p>Random Bars: {{$bars->name}}</p>
                 <button class="button-24" role="button">Press me for a drink</button>
                 <div class="wave3">
                     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
@@ -45,6 +46,7 @@
                         <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
                     </svg>
                 </div>
+                @endforeach
             </section>
 
 
