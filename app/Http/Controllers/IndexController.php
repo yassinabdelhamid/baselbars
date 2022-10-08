0005389032fc68ab7e407485b5c95106c99ce616
Yassin Abdelhamid <yassin1001@gmail.com>
2022-10-09 00:41:21 +0200

    Added limit in IndexController

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,7 +9,7 @@ class IndexController extends Controller
 {
     public function show() 
     {
-        $bar = DB::table('bars')->select('name', 'place_plz', 'price_category', 'address')->get();
+        $bar = DB::table('bars')->select('name', 'place_plz', 'price_category', 'address')->limit(1)->get();
         return view('index')->with('bar', $bar);
     }
 }
