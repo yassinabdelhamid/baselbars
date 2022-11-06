a9d3230a49b4b727c07a5c8097b4ee270088198c
Yassin Abdelhamid <yassin1001@gmail.com>
2022-11-06 21:47:33 +0100

    More Ajax tries
    Failed

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Datatables;
 
 class IndexController extends Controller
 {
@@ -11,5 +13,19 @@ class IndexController extends Controller
     {
         $bar = DB::table('bars')->select('name', 'place_plz', 'price_category', 'address')->limit(1)->get();
         return view('index')->with('bar', $bar);
+
+        $data = array();
+        while($row = $bar) {
+            $data[] = $row;
+        }
+
+        echo json_encode($data);
     }
+
+    // public function getData()
+    // {
+    //     $bars = Bar::all();
+    //     return Datatables::of($bars);
+
+    // }
 }
